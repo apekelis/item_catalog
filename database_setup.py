bc621a28a67fbb063511ea7bc1af5644813ccba9
Alan Pekelis <apekelis@gmail.com>
2019-07-20 03:33:47 -0300

    Added User methods to project

--- database_setup.py
+++ database_setup.py
@@ -12,7 +12,7 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
-    picture = Column(String(300), nullable=False)
+    picture = Column(String(300))
 
     @property
     def serialize(self):
